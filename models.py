539c1f143d9602764cbabe58afd67957b28d582c
masonbcoding <44744780+masonbcoding@users.noreply.github.com>
2021-12-06 18:48:53 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,36 +1,25 @@
 """Contains Project model class and associated code."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///portfolio.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///projects.db"
 db = SQLAlchemy(app)
 
 
 class Project(db.Model):
-    """Model class for adding and editing project information."""
-
-    __tablename__ = "project"
-
-    id = db.Column("ID", db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column("Created", db.DateTime)
     title = db.Column("Title", db.String())
-    date_completed = db.Column("Date", db.Date)
-    description = db.Column("Description", db.String())
-    skills = db.Column("Skills Practiced", db.String())
-    github = db.Column("GitHub Repo", db.String())
+    description = db.Column("Description", db.Text)
+    skills = db.Column("Skills Practiced", db.Text)
+    url = db.Column("URL", db.Text)
 
     def __repr__(self):
-        """Return printable representation of Project."""
-        return f"""\n----------
-                \rProject ID: {self.id}
-                \r----------
-                \Project Title: {self.title}
-                \r----------
-                \Date Completed: {self.date_completed}
-                \r----------
-                \Description: {self.description}
-                \r----------
-                \Skills practiced: {self.skills_practiced}
-                \r----------
-                \rGithub Repo: {self.github}
-                \r----------\n"""
+        return f"""<Project (Title: {self.title}
+                Created: {self.created}
+                Description: {self.description}
+                Skills Practiced: {self.skills}
+                URL: {self.url}
+                )"""
