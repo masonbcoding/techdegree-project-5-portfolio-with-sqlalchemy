b14ff50b3f03ef6a7095a6ff6783f09fd0ce19d0
masonbcoding <44744780+masonbcoding@users.noreply.github.com>
2021-12-07 19:19:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,6 @@ a database and displayed on the homepage.
 """
 
 
-
-   
-"""An interface for a portfolio web application.
-The main (index) page lists your projects including the project title and short
-description. Each project links to a detail page that displays the title, date,
-and description.
-The application lets the user add or edit project information. When adding or
-editing a project, the application prompts the user for title, date, skills,
-description, and a link to a repo. The results for these entries are stored in
-a database and displayed on the homepage.
-"""
-
-
 from flask import render_template, redirect, url_for, request
 from models import db, Project, app
 import datetime
